ff8b6a4b3bf3a60b656b922d55f69f8953246fa9
hadi <badawihadi64@gamil.com>
2026-01-12 14:41:35 +0200

    Add main html files

--- webpack.config.js
+++ webpack.config.js
@@ -66,22 +66,22 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: 'index.html',
       filename: 'index.html',
     }),
 
     new HtmlWebpackPlugin({
-      template: './src/main.html',
+      template: 'main.html',
       filename: 'main.html',
     }),
 
     new HtmlWebpackPlugin({
-      template: './src/main2.html',
+      template: 'main2.html',
       filename: 'main2.html',
     }),
 
     new HtmlWebpackPlugin({
-      template: './src/form.html',
+      template: 'form.html',
       filename: 'form.html',
     }),
   ],
